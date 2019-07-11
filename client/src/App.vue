aa9b2045df7ebc7a1b90a877ac80eee93b61a589
Sujoy Datta <sujoydatta26@gmail.com>
2019-07-12 02:23:46 +0530

    Demo MEVN Project
    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'app',
+   data(){
+     return {
+       msg: 'Welcome!'
+     }
+   }
+ }
+</script>
